6d55c1eda58fdc01a5d50734e5c7cb75126ab92f
hbarang <hbarangenc@gmail.com>
2019-11-11 22:10:57 +0300

    Initial Commit, add fallingThroughHole

---
+++ Assets/Scripts/FallingObjectController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjectController : MonoBehaviour
+{
+    public GameObject plane;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Hole")
+        {
+            Physics.IgnoreCollision(plane.GetComponent<Collider>(), GetComponent<Collider>());
+        }
+    }
+}
