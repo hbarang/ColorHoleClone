5f86fe7f01a61d82994b8a7c2ce3cfb9a4d628fd
hbarang <hbarangenc@gmail.com>
2019-11-14 00:36:23 +0300

    Add hole controller, add rotate through hole logic

---
+++ Assets/HoleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleController : MonoBehaviour
+{
+    public float maxHorizontalSpeed;
+    public float maxVerticalSpeed;
+    public float speedMultiplier;
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveByArrowKeys();
+    }
+
+    void MoveByArrowKeys()
+    {
+        float moveByXAxis = Input.GetAxis("Horizontal") * speedMultiplier * Time.deltaTime;
+        float moveByZAxis = Input.GetAxis("Vertical") * speedMultiplier * Time.deltaTime;
+        transform.Translate(moveByXAxis > maxHorizontalSpeed ? maxHorizontalSpeed : moveByXAxis, 0, moveByZAxis > maxVerticalSpeed ? maxVerticalSpeed : moveByZAxis);
+
+    }
+
+
+}
--- Assets/Scripts/FallingObjectController.cs
+++ Assets/Scripts/FallingObjectController.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class FallingObjectController : MonoBehaviour
 {
     public GameObject plane;
-    
+    public float titlSpeed;
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Hole")
         {
-            Physics.IgnoreCollision(plane.GetComponent<Collider>(), GetComponent<Collider>());
+            //Physics.IgnoreCollision(plane.GetComponent<Collider>(), GetComponent<Collider>());
+            RotateThroughHole(other.transform);
         }
+    }
+
+    private void RotateThroughHole(Transform holeTransform)
+    {
+        Vector2 differenceVector = new Vector2(holeTransform.position.x-transform.position.x, holeTransform.position.z-transform.position.z);
+        if(Mathf.Abs(differenceVector.x) > Mathf.Abs(differenceVector.y)){
+            RotateZ(differenceVector.x);
+        } 
+        else{
+            RotateY(differenceVector.y);
+        }
+    }
+
+    private void RotateZ(float x){
+    }
+    private void RotateY(float y){
+
     }
 }
